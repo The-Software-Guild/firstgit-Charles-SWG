ef777bdf0bad7f4a2a528e928802ff1041c806f5
Charles-SWG <87210597+Charles-SWG@users.noreply.github.com>
2021-09-08 11:03:55 -0500

    This is my First Drop

---
+++ Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+	
+	
+	public static void main (String[] args){
+		
+		if(args.length == 0)
+			System.out.println("Hello, World!");
+		else	
+			System.out.println("Hello, World! My Name is " + args[0]);
+		
+	}
+	
+	
+}
